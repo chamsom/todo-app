6f30a9e35911a0908a49b6a4b95d457f9d0f5fe2
Jae Kim <jaekimiyo@gmail.com>
2020-07-17 09:04:34 -0500

    deploying React app to gh-pages

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Study for Network+ exam",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Study Vanilla JS",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Practice Jeux d'eau on the piano",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Learn how to conditionally render using React",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Cook some kimchi pajeon for dinner",
+        completed: false
+    }
+]
+
+export default todosData
