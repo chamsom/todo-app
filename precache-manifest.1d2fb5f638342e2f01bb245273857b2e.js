77413485ad7beae7d8f725432f7b6ab58a835948
Jae Kim <jaekimiyo@gmail.com>
2020-07-17 09:05:42 -0500

    Updates

---
+++ precache-manifest.1d2fb5f638342e2f01bb245273857b2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f586c7a9230cdbddd6049c8daffe8d",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "644b4943e24bfa45ee8b",
+    "url": "/todo-app/static/css/main.0a890ee1.chunk.css"
+  },
+  {
+    "revision": "58e7156088ef2be2caa9",
+    "url": "/todo-app/static/js/2.5fc9803f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.5fc9803f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "644b4943e24bfa45ee8b",
+    "url": "/todo-app/static/js/main.db93ae43.chunk.js"
+  },
+  {
+    "revision": "cad3620733bc5b62f061",
+    "url": "/todo-app/static/js/runtime-main.bb63ee44.js"
+  }
+]);
